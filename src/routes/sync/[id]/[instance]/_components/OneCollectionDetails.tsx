bc41eca72436e76a4af52afb382984fe3840bfde
tigawanna <denniskinuthiawaweru@gmail.com>
2024-05-03 21:09:07 +0300

    clone collection

--- src/routes/sync/[id]/[instance]/_components/OneCollectionDetails.tsx
+++ src/routes/sync/[id]/[instance]/_components/OneCollectionDetails.tsx
@@ -1,9 +1,10 @@
 import { Button } from "@/components/shadcn/ui/button";
 import { ErrorOutput } from "@/components/wrappers/ErrorOutput";
 import { pbTryCatchWrapper } from "@/lib/pb/utils";
-import { useSuspenseQueries } from "@tanstack/react-query";
+import { useMutation, useSuspenseQueries } from "@tanstack/react-query";
 import { FileWarningIcon, MessageCircleWarning } from "lucide-react";
 import type Client from "pocketbase";
+import type { CollectionModel } from "pocketbase";
 import { FaClone } from "react-icons/fa";
 
 interface OneCollectionDetailsProps {
@@ -39,17 +40,26 @@ export function OneCollectionDetails({
       },
     ],
   });
+
+  const import_collection_to_secondary_mutation =useMutation({
+    mutationFn: async (collections:CollectionModel[]) => {
+      return pbTryCatchWrapper(secondaryPB.collections.import(collections,false));
+    }
+  })
   const primaryCollection = query[0].data.data;
   const secondaryCollection = query[1].data.data;
 
-  if (!secondaryCollection ) {
+  if (!secondaryCollection) {
     return (
       <div className="p-[3%] h-full min-h-[40vh] flex flex-col items-center justify-center gap-5 bg-base-300 rounded-md">
         <span className="text-xl flex gap-2 justify-center text-warning-content">
-            <FileWarningIcon className="text-warning"/>
+          <FileWarningIcon className="text-warning" />
           This collection doesn't exist in the other pocketbase instance
         </span>
-        <Button type="button" className=" flex  items-center justify-center gap-2 text-lg">
+        <Button
+          type="button"
+          className=" flex  items-center justify-center gap-2 text-lg"
+        >
           <FaClone /> clone {collectionName}
         </Button>
       </div>
